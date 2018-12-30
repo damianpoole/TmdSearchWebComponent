74795f614127504c0a5ed35d32fd4c6122b0bc82
Damian Poole <damian.poole@gmail.com>
2018-12-30 16:10:17 +0000

    ADD: Ability to close modal using escape key

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import './Modal.scss';
 
-const Modal = ({ handleClose, show, children }) => {
-    const classname = show ? 'modal visible' : 'modal';
+class Modal extends Component {
+    listenKeyboard(e) {
+        if (e.key === 'Escape') {
+            this.props.handleClose();
+        }
+    }
 
-    return (
-        <div className={classname}>
-            <section>
-                <button onClick={handleClose}>Close</button>
-                {children}
-            </section>
-        </div>
-    );
-};
+    componentDidMount() {
+        if (this.props.handleClose) {
+            window.addEventListener(
+                'keydown',
+                this.listenKeyboard.bind(this),
+                true
+            );
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.props.handleClose) {
+            window.removeEventListener(
+                'keydown',
+                this.listenKeyboard.bind(this),
+                true
+            );
+        }
+    }
+
+    render() {
+        const classname = this.props.show ? 'modal visible' : 'modal';
+
+        return (
+            <div className={classname}>
+                <section>
+                    <button onClick={this.props.handleClose}>Close</button>
+                    {this.props.children}
+                </section>
+            </div>
+        );
+    }
+}
 
 export default Modal;
